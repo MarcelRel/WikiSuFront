22f1167d79bdd9ea1335f802426288c991ffd5c8
MarcelRel <marcel.reliszka@wp.pl>
2024-04-18 11:43:15 +0200

    FE concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom";
 
-function App() {
+export const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <div className="header">
+                <div className="logo">
+                    <Link to='/'><img src="/GXO_LOGO.png" alt=""/></Link>
+                </div>
+                <div className="search_bar">
+                    <form action="">
+                        <input type="text"/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+                <div className="user_menu">
+                    <button>S</button>
+                    <button>L</button>
+                </div>
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li><Link to="/helpdesk">HelpDesk</Link></li>
+                    <li><Link to="/quries">Queries</Link></li>
+                    <li><Link to="/wow">WayOfWorking</Link></li>
+                    <li><Link to="/p1">P1-Postępowanie</Link></li>
+                    <li><Link to="/su-wiki">SU-Wiki</Link></li>
+                    <li><Link to="/wamas">Wamas</Link></li>
+                </ul>
+            </div>
+        </>
     );
 }
 
-export default App;
